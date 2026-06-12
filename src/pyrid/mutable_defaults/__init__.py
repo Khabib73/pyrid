7fbcbd77921c04e8d05c16d64dba17235e5b1662
Khabib <136262760+Khabib73@users.noreply.github.com>
2026-06-12 21:28:28 +0300

    style: improve code-style

--- src/pyrid/mutable_defaults/__init__.py
+++ src/pyrid/mutable_defaults/__init__.py
@@ -1,7 +1,7 @@
-from pyrid.mutable_defaults.md import mutable_checks, has_mutable_default, is_mutable_class
+from pyrid.mutable_defaults.md import (
+    has_mutable_default,
+    is_mutable_class,
+    mutable_checks,
+)
 
-__all__ = [
-    "mutable_checks",
-    "has_mutable_default",
-    "is_mutable_class"
-]
+__all__ = ["mutable_checks", "has_mutable_default", "is_mutable_class"]
