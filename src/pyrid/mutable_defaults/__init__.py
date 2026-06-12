1eb7a1aaac9e8f3a6e333db253aea0df27c332f2
Khabib <136262760+Khabib73@users.noreply.github.com>
2026-06-12 21:27:54 +0300

    fix: add has_mutable_default, is_mutable_class

--- src/pyrid/mutable_defaults/__init__.py
+++ src/pyrid/mutable_defaults/__init__.py
@@ -1,5 +1,7 @@
-from pyrid.mutable_defaults.md import mutable_checks
+from pyrid.mutable_defaults.md import mutable_checks, has_mutable_default, is_mutable_class
 
 __all__ = [
     "mutable_checks",
+    "has_mutable_default",
+    "is_mutable_class"
 ]
