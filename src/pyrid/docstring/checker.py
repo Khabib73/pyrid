fd2719adc8c359adaab1237c30ec2900d2c44fc7
Khabib <136262760+Khabib73@users.noreply.github.com>
2026-06-14 14:02:27 +0300

    feat(docstring): add DocstringVisitor

--- src/pyrid/docstring/checker.py
+++ src/pyrid/docstring/checker.py
@@ -1,59 +1,86 @@
 import ast
-from typing import TypeGuard
 
-from pyrid.types import FuncClassNode
+from pyrid.docstring.rules import check_d100, check_d101, check_d102, check_d103
 
 from .utils import format_docstring_msg
 
 
-def has_docstring(node: FuncClassNode) -> bool:
+class DocstringVisitor(ast.NodeVisitor):
+    def __init__(self, path: str, active_rules: set[str]) -> None:
+        self.path = path
+        self.active_rules = active_rules
+        self.errors = 0
+        self.in_class = False
+
+    def visit_Module(self, node: ast.Module) -> None:
+        """
+        Check D100: module has docstring
+        """
+        if "D100" in self.active_rules and not check_d100(node):
+            print(format_docstring_msg(self.path, node, self.path))
+            self.errors += 1
+
+        self.generic_visit(node)
+
+    def visit_ClassDef(self, node: ast.ClassDef) -> None:
+        """
+        Check D101: class has docstring
+        """
+        if "D101" in self.active_rules and not check_d101(node):
+            print(format_docstring_msg(self.path, node, node.name))
+            self.errors += 1
+
+        self.in_class = True
+        self.generic_visit(node)
+        self.in_class = False
+
+    def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
+        """
+        Check D102: function has docstring
+        """
+        if self.in_class:
+            if "D102" in self.active_rules and not check_d102(node):
+                print(format_docstring_msg(self.path, node, node.name))
+                self.errors += 1
+        else:
+            if "D103" in self.active_rules and not check_d103(node):
+                print(format_docstring_msg(self.path, node, node.name))
+                self.errors += 1
+
+        self.generic_visit(node)
+
+    def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef) -> None:
+        """
+        Check D102: async function has docstring
+        """
+        if self.in_class:
+            if "D102" in self.active_rules and not check_d102(node):
+                print(format_docstring_msg(self.path, node, node.name))
+                self.errors += 1
+        else:
+            if "D103" in self.active_rules and not check_d103(node):
+                print(format_docstring_msg(self.path, node, node.name))
+                self.errors += 1
+
+        self.generic_visit(node)
+
+
+def docstring_checks(
+    tree: ast.Module, path: str = "", active_rules: set[str] | None = None
+) -> int:
     """
-    Check if a node has a docstring.
-
-    Args:
-        node: The node to check.
-    Returns:
-        bool: True if the node has a docstring, False otherwise.
-    """
-    return ast.get_docstring(node) is not None
-
-
-def is_func_or_class(node: ast.AST) -> TypeGuard[FuncClassNode]:
-    """
-    Check if a node is a function or class.
-
-    Args:
-        node: The node to check.
-    Returns:
-        bool: True if the node is a FuncClassNode, False otherwise.
-    """
-    return isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef))
-
-
-def is_missing_docstring(node: ast.AST) -> TypeGuard[FuncClassNode]:
-    """
-    Args:
-        node: The node to check.
-    Returns:
-        bool: True if the node is a FuncClassNode and doesn't have a docstring,
-        False otherwise.
-    """
-    return is_func_or_class(node) and not has_docstring(node)
-
-
-def docstring_checks(tree: ast.Module, path: str = "") -> int:
-    """
-    Check an entire AST module for required docstrings.
+    Run docstring checks on an AST module.
 
     Args:
         tree: The AST module to check.
         path: Optional file path for error messages.
+        active_rules: Set of rule codes to check.
+                    Default to all D rules.
     Returns:
         int: The number of errors found.
     """
-    errors = 0
-    for node in ast.walk(tree):
-        if is_missing_docstring(node):
-            print(format_docstring_msg(path, node, node.name))
-            errors += 1
-    return errors
+    if active_rules is None:
+        active_rules = {"D100", "D101", "D102", "D103"}
+    visitor = DocstringVisitor(path, active_rules)
+    visitor.visit(tree)
+    return visitor.errors
