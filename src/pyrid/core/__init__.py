54f01eb7f51857b2ca68dd282c54ca4dfeaaed7c
Khabib <136262760+Khabib73@users.noreply.github.com>
2026-06-18 22:40:25 +0300

    refactor(core): add Linter for public API

--- src/pyrid/core/__init__.py
+++ src/pyrid/core/__init__.py
@@ -1,5 +1,6 @@
 from pyrid.core.base import BaseChecker
+from pyrid.core.linter import Linter
 from pyrid.core.registry import RuleRegistry
 from pyrid.core.reporter import Reporter
 
-__all__ = ["BaseChecker", "Reporter", "RuleRegistry"]
+__all__ = ["BaseChecker", "Reporter", "RuleRegistry", "Linter"]
